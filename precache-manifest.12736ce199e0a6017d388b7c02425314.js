e2f42418570db6c37db72b14d16896df87c06bba
unknown <asotjrs@gmail.com>
2020-06-24 02:18:21 +0200

    Updates

---
+++ precache-manifest.12736ce199e0a6017d388b7c02425314.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b20bc802fef84344634b7ad5f848d62",
+    "url": "/robosearcher/index.html"
+  },
+  {
+    "revision": "0d921b15e177ae82d3b6",
+    "url": "/robosearcher/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "8362ed0de7f448b99226",
+    "url": "/robosearcher/static/css/main.43fd47d1.chunk.css"
+  },
+  {
+    "revision": "0d921b15e177ae82d3b6",
+    "url": "/robosearcher/static/js/2.44e79059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robosearcher/static/js/2.44e79059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8362ed0de7f448b99226",
+    "url": "/robosearcher/static/js/main.c672acc5.chunk.js"
+  },
+  {
+    "revision": "acf08733721f71b32fc2",
+    "url": "/robosearcher/static/js/runtime-main.fa40724e.js"
+  }
+]);
