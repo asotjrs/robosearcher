b9d67a0ef9416925e410febae4ce1747d1159811
unknown <asotjrs@gmail.com>
2020-06-30 00:07:10 +0200

    resizing images

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,10 +3,10 @@ import React from "react";
 const Card=({robot})=>{
     return robot===""?null:
      <div className={" bg-light-red dib br3 pa3 ma2 grow bw2 shadow-5 tc"}>
-        <img src={'https://robohash.org/'+robot} alt={'robot'}/>
+        <img src={'https://robohash.org/'+robot+"?size=200x200"} alt={'robot'}/>
         <div>
             <h2>{robot}</h2>
-            <p>{robot.replace(' ','_')+'@gmail.com?size=200x200'}</p>
+            <p>{robot.replace(' ','_')+'@gmail.com'}</p>
         </div>
     </div>
 
