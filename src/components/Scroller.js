bdefe6596b2ad0cb70ca1f22e18be308ef7db44c
unknown <asotjrs@gmail.com>
2020-06-29 16:58:43 +0200

    deployement as a PWA

--- src/components/Scroller.js
+++ src/components/Scroller.js
@@ -1,11 +1,15 @@
-import React from "react";
+import React, {Component} from "react";
 
-const Scroller=(props)=>{
+class Scroller extends Component{
+
+shouldComponentUpdate(nextProps, nextState, nextContext) {
+  return false;
+}
+
+  render() {
   return <div style={{overflowY:'scroll' , border:'5px solid black', height:'600px'  } }>
-      {props.children}
+    {this.props.children}
   </div>;
-
-
-
-};
+}
+}
 export default Scroller;
