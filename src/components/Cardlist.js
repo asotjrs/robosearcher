672f921b157c983e6fd1e6eb333dbe9b79e3fc1d
unknown <asotjrs@gmail.com>
2020-07-01 16:15:56 +0200

    deleting some con log

--- src/components/Cardlist.js
+++ src/components/Cardlist.js
@@ -2,7 +2,6 @@ import React from "react";
 import Card from "./Card";
 
 const Cardlist=({robots})=>{
-    console.log(robots);
     const users=robots.split(",");
     const renderedList=users.map((userName,id)=>{
         return <Card robot={userName.trim()} key={id}/>;
